5764523cfd851cbad6b3b0deefa0511b8d10c5f3
Master <abrar.it@gmail.com>
2020-04-01 14:16:39 +0200

    create the repository and commit for the first time (uploading project files to github)

---
+++ app/src/main/java/com/example/smarthome/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.smarthome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void log_in(View view) {
+        Intent intent =new Intent(this,log_in.class);
+        startActivity(intent);
+
+        Log.d(LOG_TAG, "Button clicked!");
+    }
+}
